f1362b0c026bc18d272c57cdaf24525b0008343a
elias <elias.gauthier@etu.u-bordeaux.fr>
2025-11-14 23:23:07 +0100

    Fix vector addition

--- utils.py
+++ utils.py
@@ -8,4 +8,4 @@ def normalize(vector: tuple[int, int]):
     return (vector[0]/norm, vector[1]/norm)
 
 def add(vec1, vec2):
-    return (vec1[0]+vec2[0], vec1[0]+vec2[1])
+    return (vec1[0]+vec2[0], vec1[1]+vec2[1])
